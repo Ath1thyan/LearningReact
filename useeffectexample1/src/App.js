f7e4f48d38a96a91aa65016b5f9b967e3aab95ca
Ath1thyan <athitasks@gmail.com>
2024-02-15 12:24:22 +0530

    ...

--- useeffectexample1/src/App.js
+++ useeffectexample1/src/App.js
@@ -9,7 +9,8 @@ function App() {
     const weatherAPI = async () => {
       try {
         const res = await axios.get('http://api.geonames.org/findNearByWeatherJSON?lat=10.921389325&lng=77.30262575&username=arun');
-        setName(JSON.stringify(res.data));
+        const data = (JSON.stringify(res.data));
+        setName(data)
 
       } catch (error) {
         console.error('Error fetching weather data:', error);
@@ -23,7 +24,6 @@ function App() {
     <div className="App">
       <h1>Weather Data:</h1>
       <p>{name}</p>
-      {/* <p>{setName}</p> */}
       
     </div>
   );
