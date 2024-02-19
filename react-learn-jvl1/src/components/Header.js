ac933371b53dc6661742fb9789acdb21b6554ed3
Ath1thyan <athitasks@gmail.com>
2024-02-19 10:37:39 +0530

    learning react - inline css

--- react-learn-jvl1/src/components/Header.js
+++ react-learn-jvl1/src/components/Header.js
@@ -1,7 +1,15 @@
 function Header() {
+    const myStyle = {
+        color : "red", 
+        backgroundColor : "lightblue",
+        padding : "10px",
+        textAlign : "center",
+        fontFamily : "Sans-Serif"
+    };
     return (
         <div>
-            <h1>Hello styling</h1>
+            {/* <h1 style={{color : "red", backgroundColor : "lightblue"}} >Hello styling</h1> */}
+            <h1 style={myStyle} >Hello styling</h1>
             <p>Add a style</p>
         </div>
     )
