af28d6ea40e5c0a681479f91c9f6ed038bd06789
Ath1thyan <athitasks@gmail.com>
2024-02-19 09:54:45 +0530

    learnning react

--- react-learn-jvl1/src/components/Car.js
+++ react-learn-jvl1/src/components/Car.js
@@ -1,6 +1,9 @@
 function Car(props){
+    const {carInfo} = props;
+    const {brand, color} = carInfo;
     
-    const text = `Hi! I  am a ${props.carColor} ${props.carBrand} Car.`;
+    // const text = `Hi! I  am a ${props.carColor} ${props.carBrand} Car.`;
+    const text = `Hi! I  am a ${color} ${brand} Car.`;
     return (
       <h2> {text} </h2>
     )
--- react-learn-jvl1/src/components/Garage.js
+++ react-learn-jvl1/src/components/Garage.js
@@ -2,12 +2,15 @@ import Car from './Car.js';
 import Apple from './Fruit';
 
 function Garage(){
-    const brand = 'Ferrari';
-    const color = 'Black';
+    // const brand = 'Ferrari';
+    // const color = 'Black';
+
+    const carInfo = { brand : "Ferrari", color : "Black" };
     return (
       <>
         <h1>Who lives inside my  garage?</h1>
-        <Car carBrand = {brand} carColor = {color} />
+        {/* <Car carBrand = {brand} carColor = {color} /> */}
+        <Car carInfo = {carInfo} />
         <Apple/>
       </>
     )
