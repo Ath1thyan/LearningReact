c9d00d05ca352fe37a0a5ea628824b4ed9c2a929
Ath1thyan <athitasks@gmail.com>
2024-02-19 10:15:11 +0530

    learnning react

--- react-learn-jvl1/src/components/Garage.js
+++ react-learn-jvl1/src/components/Garage.js
@@ -2,17 +2,33 @@ import Car from './Car.js';
 import Apple from './Fruit';
 
 function Garage(){
+    const isOpened = false;
     // const brand = 'Ferrari';
     // const color = 'Black';
 
     const carInfo = { brand : "Ferrari", color : "Black" };
+    // const carInfo = {};
     const appleInfo = { type : "Fuji", color: "Red"};
+
+    const showCarInfo = carInfo.brand !== undefined && carInfo.color !== undefined;
     return (
       <>
         <h1>Who lives inside my  garage?</h1>
         {/* <Car carBrand = {brand} carColor = {color} /> */}
-        <Car carInfo = {carInfo} />
+
+        { showCarInfo ?
+            <Car carInfo = {carInfo} /> : null
+        }
+        { carInfo.brand !== undefined && carInfo.color !== undefined ?
+            <Car carInfo = {carInfo} /> : null
+        }
+        { showCarInfo &&
+            <Car carInfo = {carInfo} /> 
+        }
         <Apple appleInfo = {appleInfo} />
+        { isOpened ? 
+            <h2>Garage door is opened</h2> : <h2>Garage  door is closed</h2>
+        }
       </>
     )
   }
