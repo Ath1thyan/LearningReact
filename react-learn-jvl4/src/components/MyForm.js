f36762a564cb1523161b31b723b808682a2e2af2
Ath1thyan <athitasks@gmail.com>
2024-02-21 03:05:24 +0530

    learning react - Forms

--- react-learn-jvl4/src/components/MyForm.js
+++ react-learn-jvl4/src/components/MyForm.js
@@ -2,23 +2,28 @@ import { useState } from 'react';
 
 function MyForm() {
 
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
-    const [email, setEmail] = useState("");
+    // const [name, setName] = useState("");
+    // const [age, setAge] = useState("");
+    // const [email, setEmail] = useState("");
+
+    const [inputs, setInputs] = useState({});
+
     // console.log("Current State", name);
 
     function handleSubmit(e) {
         e.preventDefault();
         console.log('Form submitted');
-        console.log("Current State", name, age, email);
+        // console.log("Current State", name, age, email);
+        console.log("Current State", inputs);
     }
 
 
     return <form onSubmit={handleSubmit} >
-        <label>Name : <input type="text" onChange={(e) => {setName (e.target.value)}} /></label> <br/>
-        <label>Age : <input type="text" onChange={(e) => {setAge (e.target.value)}} /></label>
+        {/* <label>Name : <input type="text" onChange={(e) => {setName (e.target.value)}} /></label> <br/> */}
+        <label>Name : <input type="text" onChange={(e) => {setInputs( (p) => { return { ...p, name : e.target.value}})}} /></label> <br/>
+        <label>Age : <input type="text" onChange={(e) => {setInputs( (p) => { return { ...p, age : e.target.value}})}} /></label>
         <br/>
-        <label>Email : <input type="text" onChange={(e) => {setEmail (e.target.value)}} /></label>
+        <label>Email : <input type="text" onChange={(e) => {setInputs( pre => { return { ...pre, email : e.target.value}})}} /></label>
         <br/>
         <input type='submit' value='Submit Form' />
         
