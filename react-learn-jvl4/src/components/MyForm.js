5bb2548ec384f1cb09b2fce9c2e2f26ed971ead3
Ath1thyan <athitasks@gmail.com>
2024-02-21 02:28:56 +0530

    learning react - Forms

--- react-learn-jvl4/src/components/MyForm.js
+++ react-learn-jvl4/src/components/MyForm.js
@@ -1,8 +1,26 @@
-
+import { useState } from 'react';
 
 function MyForm() {
-    return <form>
-        <label>Name : <input type="text" /></label>
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [email, setEmail] = useState("");
+    // console.log("Current State", name);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('Form submitted');
+        console.log("Current State", name, age, email);
+    }
+
+
+    return <form onSubmit={handleSubmit} >
+        <label>Name : <input type="text" onChange={(e) => {setName (e.target.value)}} /></label> <br/>
+        <label>Age : <input type="text" onChange={(e) => {setAge (e.target.value)}} /></label>
+        <br/>
+        <label>Email : <input type="text" onChange={(e) => {setEmail (e.target.value)}} /></label>
+        <br/>
+        <input type='submit' value='Submit Form' />
         
     </form>
 }
