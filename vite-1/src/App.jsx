d99948e0a81c6f91cde589b34f72c99b89c19a25
Ath1thyan <athitasks@gmail.com>
2024-06-03 06:22:56 +0530

    ...

---
+++ vite-1/src/App.jsx
@@ -0,0 +1,12 @@
+import LearnComponents from './components/LearnComponents'
+
+function App() {
+
+  return (
+    <>
+      <LearnComponents name="Athi" age={23} gender={true} />
+    </>
+  )
+}
+
+export default App
---
+++ vite-1/src/components/LearnComponents.jsx
@@ -0,0 +1,36 @@
+import propTypes from 'prop-types'
+
+const LearnComponents = (props) => {
+  return (
+    <div className="student">
+    <table>
+        <tr>
+            <th>Name</th>
+            <td>{props.name}</td>
+        </tr>
+        <tr>
+            <th>Age</th>
+            <td>{props.age}</td>
+        </tr>
+        <tr>
+            <th>Gender</th>
+            <td>{props.gender ? "Male" : "Female"}</td>
+        </tr>
+    </table>
+    </div>
+  )
+}
+
+LearnComponents.propTypes ={
+    name: propTypes.string.isRequired,
+    age: propTypes.number.isRequired,
+    gender: propTypes.bool.isRequired,
+}
+
+LearnComponents.defaultProps = {
+    gender: "na",
+    age: 0,
+    name: "na"
+}
+
+export default LearnComponents
